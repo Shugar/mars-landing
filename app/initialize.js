1f07f323463c6972d454d11aee17e03136f5d112
Tigran Tumasov <shugar348@gmail.com>
2017-03-09 04:07:14 +0300

    Init full project without links

---
+++ app/initialize.js
@@ -0,0 +1,8 @@
+import $ from "jquery";
+
+$(document).ready(() => {
+  $('a[href^="#"]').on('click', function() {
+    let el = $(this).attr('href');
+    $('body').animate({scrollTop: $(el).offset().top}, 500);
+  });
+});
